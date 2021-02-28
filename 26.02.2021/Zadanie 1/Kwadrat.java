0850ea63bb62affb51ad4a9a804cfc75667d5e4f
PawelRichert13 <79452480+PawelRichert13@users.noreply.github.com>
2021-02-28 14:39:03 +0100

    Add files via upload

---
+++ 26.02.2021/Zadanie 1/Kwadrat.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Kwadrat
+{
+    int bok;
+    int obwod;
+    int pole;
+
+    public Kwadrat(int bok)
+    {
+     this.bok = bok;
+     this.obwod = bok+bok+bok+bok;
+     this.pole = bok * bok;
+     System.out.println("obwod = " + this.obwod);
+
+
+    }
+
+
+}
